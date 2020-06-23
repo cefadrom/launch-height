1d32a0bff43911f9fcc50a77d770632e1331b44e
Julien <cefadrom1@gmail.com>
2020-06-23 11:04:04 +0200

    Errors and params improvements

--- src/utils/SensorData.ts
+++ src/utils/SensorData.ts
@@ -9,11 +9,20 @@ export default class SensorData {
     private noDataTimeout?: number = undefined;
     private throttleValue: number | null;
     private sensorCallback: (ev: DeviceMotionEvent) => any;
-    public excludeGravity = true;
+    private consecutiveErrors = 0;
+    public excludeGravity: boolean;
 
-    constructor(throttleValue = null) {
+    /**
+     * @param throttleValue Optional throttle value (in times per second)
+     * @param excludeGravity Exclude the gravity in the returned values
+     */
+    constructor({ throttle: throttleValue = null, excludeGravity = false }: { throttle?: null | number, excludeGravity?: boolean } = {}) {
         this.throttleValue = throttleValue;
+        this.excludeGravity = excludeGravity;
         this.handleSensorData = this.handleSensorData.bind(this);
+        this.startListening = this.startListening.bind(this);
+        this.stopListening = this.stopListening.bind(this);
+        this.setThrottle = this.setThrottle.bind(this);
         this.sensorCallback = throttle(this.handleSensorData, 0);
     }
 
@@ -37,13 +46,21 @@ export default class SensorData {
             data = ev.acceleration;
         else
             data = ev.accelerationIncludingGravity;
-        if (!data || data.x === null || data.y === null || data.z === null)
-            return this.handleError(`Impossible d'obtenir les données de l'accéléromètre`);
-
-        this.callback!(data as Cordinates);
+        if (!data || data.x === null || data.y === null || data.z === null) {
+            this.consecutiveErrors++;
+            if (this.consecutiveErrors > 10) {
+                return this.handleError(!data
+                    ? `L'accéléromètre n'a retourné aucune donnée 10 fois de suite.`
+                    : `L'accéléromètre a retourné des données incomplètes 10 fois de suite.`,
+                );
+            }
+        } else {
+            this.callback!(data as Cordinates);
+        }
     }
 
     private handleError(err: string) {
+        clearInterval(this.noDataTimeout);
         this.stopListening();
         this.checkCallback();
         this.callback!({ x: 0, y: 0, z: 0 }, err);
@@ -51,7 +68,7 @@ export default class SensorData {
 
     public startListening() {
         this.noDataTimeout = setTimeout(
-            () => this.handleError(`L'accéléromètre n'a retourné aucune donnée`),
+            () => this.handleError(`L'accéléromètre n'a retourné aucune donnée après une seconde.`),
             1000,
         );
 
@@ -64,7 +81,7 @@ export default class SensorData {
                 this.handleError(e);
             }
         } else {
-            this.handleError(`Impossible d'accéder à l'accéléromètre`);
+            this.handleError(`Impossible d'accéder à l'accéléromètre.`);
         }
     }
 
@@ -78,7 +95,6 @@ export default class SensorData {
 
     public setThrottle(throttle: number | null) {
         this.throttleValue = throttle;
-        console.log(this.throttleValue, 1000 / this.throttleValue!);
         this.stopListening();
         this.startListening();
     }
